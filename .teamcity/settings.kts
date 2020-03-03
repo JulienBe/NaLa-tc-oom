f77bc8df1f7976dc594c0ce7f7e5e1f3b2207d7a
teamcity <teamcity@TeamCity>
2020-03-03 16:24:58 +0100

    TeamCity change in 'Oom-Lab-Jobs' project: requirements of 'oom deploy clamp' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -114,10 +114,6 @@ object OomLabJobs_OomDeployClamp : BuildType({
         vcs {
         }
     }
-
-    requirements {
-        contains("system.agent.name", "ip_10.0.0.151")
-    }
 })
 
 object OomLabJobs_update_oom : BuildType({
